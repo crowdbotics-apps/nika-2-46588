8fa8ef8dcad369fb96babb790f5eba4ac6186229
Crowdbotics <team@crowdbotics.com>
2024-02-22 21:39:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nika-2-46588",
+  url: "https://nika-2-46588.botics.co",
+  api: "https://nika-2-46588.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
